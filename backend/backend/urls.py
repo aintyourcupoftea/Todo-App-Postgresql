abfac0e3cb6d9ad048fda0fa832e71e8a3e2441a
TangadeSandesh <tangadesandesh2001@gmail.com>
2024-07-08 05:06:48 +0530

    pushed both frontend and backend

---
+++ backend/backend/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from todo import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

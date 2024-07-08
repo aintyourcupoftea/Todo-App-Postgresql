e333c94615509e292c0c100630c4a479c974c44d
Amit Gavali <79439692+aintyourcupoftea@users.noreply.github.com>
2024-07-09 00:23:56 +0530

    Update build_files.sh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -4,10 +4,10 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 # Install project dependencies
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Apply database migrations (within the virtual environment)
-python manage.py migrate
+python3 manage.py migrate
 
 # Collect static files 
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input

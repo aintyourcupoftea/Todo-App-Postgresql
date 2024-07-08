24ab48e3f5b4f952e345906c2a1476c5fc7f7ea0
TangadeSandesh <tangadesandesh2001@gmail.com>
2024-07-08 23:52:13 +0530

    build_files.sh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export PATH="/usr/local/bin:$PATH"
+source ~/.bash_profile
+
 # build_files.sh
 echo "Installing Stuff...."
 python3 -m pip install -r requirements.txt
@@ -7,3 +10,5 @@ python3 -m pip install -r requirements.txt
 # make migrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
+
+
